8e427e85aad34267881eaee180c90839c92aa382
Micah Kinzelman <mkinzelman@believer.gg>
2024-06-06 10:45:43 -0700

    chore(birdie): character metadata added and working

--- birdie/src-tauri/src/metadata/character.rs
+++ birdie/src-tauri/src/metadata/character.rs
@@ -7,13 +7,6 @@ pub struct CharacterMetadata {
     pub code_name: String,
     pub character_name: String,
     pub rigs: HashMap<String, String>,
-}
-
-#[derive(Debug, Default, Clone, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct RigsMetadata {
-    pub body: String,
-    pub face: String,
-    pub weapon_primary: String,
-    pub weapon_secondary: String,
+    pub animations: HashMap<String, String>,
+    pub meshes: HashMap<String, String>,
 }
--- birdie/src/lib/components/metadata/CharacterCard.svelte
+++ birdie/src/lib/components/metadata/CharacterCard.svelte
@@ -12,6 +12,12 @@
 
 	let editingMetadata: boolean = false;
 	let updatingMetadata: boolean = false;
+	let newRigEntry: string = '';
+	let tempRigData: [string, string][];
+	let tempAnimationData: [string, string][];
+	let newAnimationEntry: string = '';
+	let tempMeshData: [string, string][];
+	let newMeshEntry: string = '';
 
 	const handleEditMetadata = () => {
 		if (!metadata) return;
@@ -23,17 +29,21 @@
 		if (!tempMetadata.character) {
 			tempMetadata.character = {
 				codeName: '',
-				characterName: ''
+				characterName: '',
+				rigs: {},
+				animations: {},
+				meshes: {}
 			};
 		}
 
+		if (tempMetadata.character) {
+			// Temporary array from 'rigs' so {#each} in svelte can iterate over it
+			tempRigData = Object.entries(tempMetadata.character.rigs);
+			tempAnimationData = Object.entries(tempMetadata.character.animations);
+			tempMeshData = Object.entries(tempMetadata.character.meshes);
+		}
+
 		editingMetadata = true;
-	};
-
-	const handleAddRig = () => {};
-
-	const handleRemoveRig = (rigName: string) => {
-		tempMetadata.character.rigs.delete(rigName);
 	};
 
 	const cancelEditMetadata = () => {
@@ -41,11 +51,74 @@
 		$enableGlobalSearch = true;
 	};
 
+	const handleAddRig = (rigName: string) => {
+		if (rigName !== '') {
+			tempRigData.push([rigName, '']);
+			tempRigData = [...tempRigData]; // Force UI update
+			newRigEntry = '';
+		}
+	};
+
+	const handleRemoveRig = (rigName: string) => {
+		const index = tempRigData.findIndex((x) => x[0] === rigName);
+		if (index > -1) {
+			tempRigData.splice(index, 1);
+			tempRigData = [...tempRigData]; // Force UI update
+		}
+	};
+
+	const handleAddAnimation = (directoryName: string) => {
+		if (directoryName !== '') {
+			tempAnimationData.push([directoryName, '']);
+			tempAnimationData = [...tempAnimationData]; // Force UI update
+			newAnimationEntry = '';
+		}
+	};
+
+	const handleRemoveAnimation = (directoryName: string) => {
+		const index = tempAnimationData.findIndex((x) => x[0] === directoryName);
+		if (index > -1) {
+			tempAnimationData.splice(index, 1);
+			tempAnimationData = [...tempAnimationData]; // Force UI update
+		}
+	};
+
+	const handleAddMesh = (meshName: string) => {
+		if (meshName !== '') {
+			tempMeshData.push([meshName, '']);
+			tempMeshData = [...tempMeshData]; // Force UI update
+			newMeshEntry = '';
+		}
+	};
+
+	const handleRemoveMesh = (meshName: string) => {
+		const index = tempMeshData.findIndex((x) => x[0] === meshName);
+		if (index > -1) {
+			tempMeshData.splice(index, 1);
+			tempMeshData = [...tempMeshData]; // Force UI update
+		}
+	};
+
 	const saveMetadata = async () => {
 		if (!tempMetadata) return;
 
 		updatingMetadata = true;
 		// Save the metadata
+		// On Save fill 'rigs' Record from the Temporary Array
+		if (tempMetadata.character) {
+			tempMetadata.character.rigs = {};
+			for (const [name, path] of tempRigData) {
+				tempMetadata.character.rigs[name] = path;
+			}
+			tempMetadata.character.animations = {};
+			for (const [name, path] of tempAnimationData) {
+				tempMetadata.character.animations[name] = path;
+			}
+			tempMetadata.character.meshes = {};
+			for (const [name, path] of tempMeshData) {
+				tempMetadata.character.meshes[name] = path;
+			}
+		}
 
 		try {
 			await onMetadataSaved(tempMetadata);
@@ -84,33 +157,105 @@
 	</div>
 	<div>
 		{#if editingMetadata && tempMetadata && tempMetadata.character}
-			<div class="flex h=20 items-center gap-2 mb-2 justify-between">
-				<Label for="codeName" class="mb-1">Code_Name</Label>
-				<Input label="CodeName" bind:value={tempMetadata.character.codeName} />
+			<div class="flex items-center gap-2 mb-2 justify-between">
+				<Label for="codeName" class="mb-1 w-1/12">Code Name</Label>
+				<Input label="CodeName" class="h-9" bind:value={tempMetadata.character.codeName} />
 			</div>
 
 			<div class="flex items-center gap-2 mb-2 justify-between">
-				<Label for="characterName" class="mb-1">Name</Label>
-				<Input label="CharacterName" bind:value={tempMetadata.character.characterName} />
+				<Label for="characterName" class="mb-1 w-1/12">Name</Label>
+				<Input
+					label="CharacterName"
+					class="h-9"
+					bind:value={tempMetadata.character.characterName}
+				/>
 			</div>
 
-			<div class="flex items-center gap-2 mb-2 justify-between">
-				<Label for="rig" class="mb-1">Rigs</Label>
-				<Button size="xs" on:click={handleAddRig}><PlusSolid class="w-4 h-4" /></Button>
+			<div class="flex items-center gap-2 mt-6 mb-2">
+				<Label for="rig" class="mb-1 w-1/2">RIGS</Label>
+				<div class="flex items-center gap-2 w-1/2">
+					<Label for="newRigName" class="font-bold w-3/4 mb-1" style="text-align: right"
+						>New Rig File</Label
+					>
+					<Input label="rigPath" class="h-8 w-1/3" bind:value={newRigEntry} />
+					<Button size="xs" on:click={() => handleAddRig(newRigEntry)}
+						><PlusSolid class="w-3 h-4" /></Button
+					>
+				</div>
 			</div>
 
-			{#each Object.entries(tempMetadata.character.rigs) as [rigName, _]}
-				<div class="flex gap-2">
-					<Label for="rigName" class="mb-1">{rigName}</Label>
-					<Input label="rigPath" bind:value={tempMetadata.character.rigs[rigName]} />
-					<Button size="xs" on:click={() => handleRemoveRig(rigName)}
-						><CloseSolid class="w-4 h-4" /></Button
+			{#each tempRigData as [rigName, rigPath]}
+				<div class="flex items-center justify-start gap-2 mb-0.5">
+					<Label for="rigName" class="mb-1 ml-4 w-1/12">{rigName}</Label>
+					<Input label="rigPath" class="h-8" bind:value={rigPath} />
+					<Button
+						size="xs"
+						class="my-1 dark:bg-red-800 hover:dark:bg-red-900"
+						on:click={() => handleRemoveRig(rigName)}
+					>
+						<CloseSolid class="w-3 h-3" /></Button
+					>
+				</div>
+			{/each}
+
+			<div class="flex items-center gap-2 mt-6 mb-2">
+				<Label for="rig" class="mb-1 w-1/2">ANIMATIONS</Label>
+				<div class="flex items-center gap-2 w-1/2">
+					<Label for="newDirectoryName" class="font-bold w-3/4 mb-1" style="text-align: right"
+						>New Animation Directory</Label
+					>
+					<Input label="directoryPath" class="h-8 w-1/3" bind:value={newAnimationEntry} />
+					<Button size="xs" on:click={() => handleAddAnimation(newAnimationEntry)}
+						><PlusSolid class="w-3 h-4" /></Button
+					>
+				</div>
+			</div>
+
+			{#each tempAnimationData as [directoryName, directoryPath]}
+				<div class="flex items-center justify-start gap-2 mb-0.5">
+					<Label for="directoryName" class="mb-1 ml-4 w-1/12">{directoryName}</Label>
+					<Input label="directoryPath" class="h-8" bind:value={directoryPath} />
+					<Button
+						size="xs"
+						class="my-1 dark:bg-red-800 hover:dark:bg-red-900"
+						on:click={() => handleRemoveAnimation(directoryName)}
+					>
+						<CloseSolid class="w-3 h-3" /></Button
+					>
+				</div>
+			{/each}
+
+			<div class="flex items-center gap-2 mt-6 mb-2">
+				<Label for="rig" class="mb-1 w-1/2">MESHES</Label>
+				<div class="flex items-center gap-2 w-1/2">
+					<Label for="newMeshName" class="font-bold w-3/4 mb-1" style="text-align: right"
+						>New Mesh File</Label
+					>
+					<Input label="meshPath" class="h-8 w-1/3" bind:value={newMeshEntry} />
+					<Button size="xs" on:click={() => handleAddMesh(newMeshEntry)}
+						><PlusSolid class="w-3 h-4" /></Button
+					>
+				</div>
+			</div>
+
+			{#each tempMeshData as [meshName, meshPath]}
+				<div class="flex items-center justify-start gap-2 mb-0.5">
+					<Label for="meshName" class="mb-1 ml-4 w-1/12">{meshName}</Label>
+					<Input label="meshPath" class="h-8" bind:value={meshPath} />
+					<Button
+						size="xs"
+						class="my-1 dark:bg-red-800 hover:dark:bg-red-900"
+						on:click={() => handleRemoveMesh(meshName)}
+					>
+						<CloseSolid class="w-3 h-3" /></Button
 					>
 				</div>
 			{/each}
 		{:else}
-			CodeName: {metadata?.character?.codeName ?? ''}
 			Name: {metadata?.character?.characterName ?? ''}
+			<div>
+				CodeName: {metadata?.character?.codeName ?? ''}
+			</div>
 		{/if}
 	</div>
 </Card>
