c97b5724d8783f6f77aa6ecf2d8d430faadd447d
Andrew Rudoi <arudoi@believer.gg>
2024-07-02 14:45:30 -0700

    fix(core): sort releases based on semver

--- friendshipper/src-tauri/src/system/update.rs
+++ friendshipper/src-tauri/src/system/update.rs
@@ -47,28 +47,53 @@ where
         .send()
         .await?;
 
-    let latest: Option<String> = releases
+    let mut latest = releases
         .into_iter()
         .filter_map(|release| {
             if release.draft || release.prerelease {
                 return None;
             }
 
+            // if release doesn't match the format app_name-vX.Y.Z, skip it
+            if !release.tag_name.starts_with(&format!("{}-v", app_name)) {
+                return None;
+            }
+
+            // get semver
+            let version = release
+                .tag_name
+                .strip_prefix(&format!("{}-v", app_name))
+                .unwrap();
+            if semver::Version::parse(version).is_err() {
+                return None;
+            }
+
             let tag_name = release.tag_name.clone();
-            if release.tag_name.starts_with(&app_name)
-                && release
-                    .assets
-                    .iter()
-                    .any(|asset| asset.name == format!("{}{}", &app_name, BIN_SUFFIX))
+            if release
+                .assets
+                .iter()
+                .any(|asset| asset.name == format!("{}{}", &app_name, BIN_SUFFIX))
             {
                 return Some(tag_name);
             }
 
             None
         })
-        .next();
+        .collect::<Vec<String>>();
 
-    match latest {
+    // sort by semver
+    latest.sort_by(|a, b| {
+        // we can unwrap because we asserted this format earlier
+        let a = a.strip_prefix(&format!("{}-v", &app_name)).unwrap();
+        let b = b.strip_prefix(&format!("{}-v", &app_name)).unwrap();
+        let a = semver::Version::parse(a).unwrap();
+        let b = semver::Version::parse(b).unwrap();
+
+        // reverse it
+        b.cmp(&a)
+    });
+
+    match latest.first() {
         Some(latest) => Ok(latest
             .strip_prefix(&format!("{}-v", &app_name))
             .unwrap()
