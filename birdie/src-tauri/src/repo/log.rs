8ae41e673383337b9447fd25168235cd050af278
Andrew Rudoi <arudoi@believer.gg>
2024-07-23 22:29:12 -0700

    chore(friendshipper): don't block log commands behind status commands

--- birdie/src-tauri/src/repo/log.rs
+++ birdie/src-tauri/src/repo/log.rs
@@ -7,7 +7,6 @@ use serde::Deserialize;
 
 use ethos_core::operations::{LogOp, LogResponse};
 use ethos_core::types::errors::CoreError;
-use ethos_core::worker::{NoOp, TaskSequence};
 
 use crate::state::AppState;
 
@@ -18,12 +17,6 @@ pub struct LogParams {
 
     #[serde(default)]
     pub use_remote: bool,
-
-    // Whether to force a fetch before getting the log
-    // This parameter does not get read
-    #[allow(dead_code)]
-    #[serde(default)]
-    pub update: bool,
 }
 
 fn default_limit() -> usize {
@@ -34,14 +27,6 @@ pub async fn log_handler(
     State(state): State<Arc<AppState>>,
     params: Query<LogParams>,
 ) -> Result<Json<LogResponse>, CoreError> {
-    // Make sure we wait for any queued updates
-    let (tx, rx) = tokio::sync::oneshot::channel::<Option<anyhow::Error>>();
-    let mut sequence = TaskSequence::new().with_completion_tx(tx);
-
-    sequence.push(Box::new(NoOp));
-    let _ = state.operation_tx.send(sequence).await;
-    let _ = rx.await;
-
     let log_op = LogOp {
         limit: params.limit,
         use_remote: params.use_remote,
