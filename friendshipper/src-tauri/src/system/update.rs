360efe2a46a3358dd342632963f92aaa4b704c90
Andrew Rudoi <arudoi@believer.gg>
2024-08-13 16:24:52 -0700

    fix(friendshipper): correctly sort releases when self-updating

--- friendshipper/src-tauri/src/system/update.rs
+++ friendshipper/src-tauri/src/system/update.rs
@@ -38,6 +38,98 @@ where
         Octocrab::builder().build()?
     };
 
+    let latest = get_latest(&octocrab).await?;
+
+    Ok(latest
+        .tag_name
+        .strip_prefix(&format!("{}-v", APP_NAME.to_lowercase()))
+        .unwrap()
+        .to_string())
+}
+
+pub async fn run_update<T>(State(state): State<AppState<T>>) -> Result<(), CoreError>
+where
+    T: EngineProvider,
+{
+    info!("Running update");
+    let token = state.app_config.read().github_pat.clone();
+    let octocrab = if let Some(ref token) = token {
+        Octocrab::builder().personal_token(token.clone()).build()?
+    } else {
+        Octocrab::builder().build()?
+    };
+
+    let latest = get_latest(&octocrab).await?;
+    let app_name = APP_NAME.to_lowercase();
+
+    let asset = latest
+        .assets
+        .iter()
+        .find(|asset| asset.name == format!("{}{}", &app_name, BIN_SUFFIX));
+
+    match asset {
+        Some(asset) => {
+            // download release
+            info!(
+                "https://api.github.com/repos/{}/{}/releases/assets/{}",
+                REPO_OWNER, REPO_NAME, asset.id
+            );
+
+            let client = reqwest::Client::new();
+            let response = client
+                .get(format!(
+                    "https://api.github.com/repos/{}/{}/releases/assets/{}",
+                    REPO_OWNER, REPO_NAME, asset.id
+                ))
+                .header("Accept", "application/octet-stream")
+                .header("User-Agent", app_name);
+            let mut response = if let Some(token) = token {
+                response
+                    .header("Authorization", format!("Bearer {}", token))
+                    .send()
+                    .await?
+            } else {
+                response.send().await?
+            };
+
+            if response.status().is_client_error() {
+                let text = response.text().await?;
+                error!("Error downloading asset: {:?}", text.clone());
+                return Err(CoreError(anyhow!("Error downloading asset: {:?}", text)));
+            }
+
+            let exe_path = match std::env::current_exe() {
+                Ok(path) => path,
+                Err(e) => {
+                    return Err(anyhow!("Error getting current exe path: {:?}", e).into());
+                }
+            };
+
+            let tmp_path = format!("{}_tmp", exe_path.to_str().unwrap());
+            info!("Downloading to: {}", tmp_path);
+
+            let mut file = File::create(tmp_path.clone())?;
+            while let Some(chunk) = response.chunk().await? {
+                file.write_all(&chunk)?;
+            }
+
+            match self_replace::self_replace(&tmp_path) {
+                Ok(_) => {
+                    info!("Updated exe");
+                    fs::remove_file(&tmp_path)?;
+                    Ok(())
+                }
+                Err(e) => {
+                    error!("Error replacing exe: {:?}", e);
+                    Err(anyhow!("Error replacing exe: {:?}", e).into())
+                }
+            }
+        }
+        None => Err(anyhow!("No asset found").into()),
+    }
+}
+
+async fn get_latest(octocrab: &Octocrab) -> anyhow::Result<Release> {
     let app_name = APP_NAME.to_lowercase();
 
     let releases = octocrab
@@ -68,24 +160,29 @@ where
                 return None;
             }
 
-            let tag_name = release.tag_name.clone();
             if release
                 .assets
                 .iter()
                 .any(|asset| asset.name == format!("{}{}", &app_name, BIN_SUFFIX))
             {
-                return Some(tag_name);
+                return Some(release);
             }
 
             None
         })
-        .collect::<Vec<String>>();
+        .collect::<Vec<Release>>();
 
     // sort by semver
     latest.sort_by(|a, b| {
         // we can unwrap because we asserted this format earlier
-        let a = a.strip_prefix(&format!("{}-v", &app_name)).unwrap();
-        let b = b.strip_prefix(&format!("{}-v", &app_name)).unwrap();
+        let a = a
+            .tag_name
+            .strip_prefix(&format!("{}-v", &app_name))
+            .unwrap();
+        let b = b
+            .tag_name
+            .strip_prefix(&format!("{}-v", &app_name))
+            .unwrap();
         let a = semver::Version::parse(a).unwrap();
         let b = semver::Version::parse(b).unwrap();
 
@@ -94,123 +191,7 @@ where
     });
 
     match latest.first() {
-        Some(latest) => Ok(latest
-            .strip_prefix(&format!("{}-v", &app_name))
-            .unwrap()
-            .to_string()),
-        None => Err(anyhow!("No latest version found").into()),
-    }
-}
-
-pub async fn run_update<T>(State(state): State<AppState<T>>) -> Result<(), CoreError>
-where
-    T: EngineProvider,
-{
-    info!("Running update");
-    let token = state.app_config.read().github_pat.clone();
-    let octocrab = if let Some(ref token) = token {
-        Octocrab::builder().personal_token(token.clone()).build()?
-    } else {
-        Octocrab::builder().build()?
-    };
-
-    let app_name = APP_NAME.to_lowercase();
-
-    let releases = octocrab
-        .repos(REPO_OWNER, REPO_NAME)
-        .releases()
-        .list()
-        .send()
-        .await?;
-
-    let latest: Option<Release> = releases
-        .into_iter()
-        .filter_map(|release| {
-            if release.draft || release.prerelease {
-                return None;
-            }
-
-            if release.tag_name.starts_with(&app_name)
-                && release
-                    .assets
-                    .iter()
-                    .any(|asset| asset.name == format!("{}{}", &app_name, BIN_SUFFIX))
-            {
-                return Some(release);
-            }
-
-            None
-        })
-        .next();
-
-    match latest {
-        Some(latest) => {
-            let asset = latest
-                .assets
-                .iter()
-                .find(|asset| asset.name == format!("{}{}", &app_name, BIN_SUFFIX));
-
-            match asset {
-                Some(asset) => {
-                    // download release
-                    info!(
-                        "https://api.github.com/repos/{}/{}/releases/assets/{}",
-                        REPO_OWNER, REPO_NAME, asset.id
-                    );
-
-                    let client = reqwest::Client::new();
-                    let response = client
-                        .get(format!(
-                            "https://api.github.com/repos/{}/{}/releases/assets/{}",
-                            REPO_OWNER, REPO_NAME, asset.id
-                        ))
-                        .header("Accept", "application/octet-stream")
-                        .header("User-Agent", &app_name);
-                    let mut response = if let Some(token) = token {
-                        response
-                            .header("Authorization", format!("Bearer {}", token))
-                            .send()
-                            .await?
-                    } else {
-                        response.send().await?
-                    };
-
-                    if response.status().is_client_error() {
-                        let text = response.text().await?;
-                        error!("Error downloading asset: {:?}", text.clone());
-                        return Err(CoreError(anyhow!("Error downloading asset: {:?}", text)));
-                    }
-
-                    let exe_path = match std::env::current_exe() {
-                        Ok(path) => path,
-                        Err(e) => {
-                            return Err(anyhow!("Error getting current exe path: {:?}", e).into());
-                        }
-                    };
-
-                    let tmp_path = format!("{}_tmp", exe_path.to_str().unwrap());
-                    info!("Downloading to: {}", tmp_path);
-
-                    let mut file = File::create(tmp_path.clone())?;
-                    while let Some(chunk) = response.chunk().await? {
-                        file.write_all(&chunk)?;
-                    }
-
-                    match self_replace::self_replace(&tmp_path) {
-                        Ok(_) => {
-                            info!("Updated exe");
-                            fs::remove_file(&tmp_path)?;
-                            Ok(())
-                        }
-                        Err(e) => {
-                            error!("Error replacing exe: {:?}", e);
-                            Err(anyhow!("Error replacing exe: {:?}", e).into())
-                        }
-                    }
-                }
-                None => Err(anyhow!("No asset found").into()),
-            }
-        }
-        None => Err(anyhow!("No latest version found").into()),
+        Some(latest) => Ok(latest.clone()),
+        None => Err(anyhow!("No latest version found")),
     }
 }
