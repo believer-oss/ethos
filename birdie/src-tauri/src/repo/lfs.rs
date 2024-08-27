822cd4d9febdef7f8eb21abd7729074da27719fd
Sylvia Chen <schen@believer.gg>
2024-08-27 09:46:00 -0700

    fix(birdie): flatten directories before writing to lfs.fetchincude

--- birdie/src-tauri/src/repo/lfs.rs
+++ birdie/src-tauri/src/repo/lfs.rs
@@ -1,4 +1,5 @@
 use anyhow::anyhow;
+use std::fs;
 use std::path::PathBuf;
 use std::sync::Arc;
 
@@ -21,11 +22,19 @@ pub async fn download_files(
     State(state): State<Arc<AppState>>,
     Json(request): Json<DownloadFilesRequest>,
 ) -> Result<(), CoreError> {
-    // join file paths by comma
-    let include_arg = request.files.join(",");
-
+    // create params for setting fetch include
     let repo_path = state.app_config.read().repo_path.clone();
-    set_fetch_include(repo_path, &include_arg).await?;
+    let mut fetch_include_paths = "".to_string();
+    for file_path in request.files.iter() {
+        // recursively flatten directory into all child files
+        let full_path = PathBuf::from(repo_path.clone()).join(file_path);
+        let local_path = PathBuf::from(file_path.clone());
+        fetch_include_paths.push_str(flatten_path(full_path, local_path).as_str());
+    }
+    set_fetch_include(repo_path, &fetch_include_paths).await?;
+
+    // format for command line lfs pull format
+    let include_arg = request.files.join(",");
 
     state
         .git()
@@ -38,12 +47,30 @@ pub async fn download_files(
     Ok(())
 }
 
+pub fn flatten_path(full_path: PathBuf, local_path: PathBuf) -> String {
+    let mut flat_paths = String::from("");
+    if full_path.is_dir() {
+        for child in fs::read_dir(full_path).unwrap() {
+            let child = child.unwrap();
+            let child_path = child.path();
+            let child_local_path = local_path.join(child.file_name());
+            flat_paths.push_str(flatten_path(child_path, child_local_path).as_str());
+        }
+    } else {
+        // append string with local file path so it's easier for frontend to string match
+        flat_paths.push_str(local_path.to_str().unwrap());
+        flat_paths.push(',');
+    }
+    flat_paths
+}
+
 pub async fn set_fetch_include(repo_path: String, paths: &str) -> Result<(), CoreError> {
     let config_path = PathBuf::from(repo_path).join(".git/config");
     let mut git_config =
         gix_config::File::from_path_no_includes(config_path.clone(), Source::Local)?;
 
-    let mut all_paths = paths.to_string();
+    let mut all_paths = paths.replace('\\', "/").to_string();
+
     match git_config.raw_value("lfs.fetchinclude") {
         Ok(value) => {
             all_paths.push(',');
--- birdie/src/routes/+page.svelte
+++ birdie/src/routes/+page.svelte
@@ -445,20 +445,6 @@
 		showSearchModal = false;
 	};
 
-	const inFetchInclude = (path: string) => {
-		for (let i = 0; i <= fetchIncludeList.length; i += 1) {
-			if (/\.[^/]+$/.test(fetchIncludeList[i]) && fetchIncludeList[i] === path) {
-				// if the path ends with a file extension (.*), do a direct string match
-				return true;
-			}
-			if (path.startsWith(fetchIncludeList[i])) {
-				// path is a directory
-				return true;
-			}
-		}
-		return false;
-	};
-
 	void listen('refresh-files', () => {
 		void refreshFiles();
 	});
@@ -554,10 +540,12 @@
 											/>
 										</TableBodyCell>
 										<TableBodyCell class="p-2 w-4">
-											{#if inFetchInclude(`${$currentRoot}/${file.name}`)}
-												<HeartSolid class="w-4 h-4 text-green-500" />
-											{:else}
-												<HeartOutline class="w-4 h-4 text-gray-500" />
+											{#if file.fileType === FileType.File}
+												{#if fetchIncludeList.includes(`${$currentRoot}/${file.name}`)}
+													<HeartSolid class="w-4 h-4 text-green-500" />
+												{:else}
+													<HeartOutline class="w-4 h-4 text-gray-500" />
+												{/if}
 											{/if}
 										</TableBodyCell>
 										<TableBodyCell class="p-2">
@@ -686,7 +674,9 @@
 							<div class="flex gap-2 w-full dark:text-white">
 								<span class="w-20">Favorited:</span>
 								<span class="dark:text-primary-400 w-64"
-									>{!inFetchInclude(`${$currentRoot}/${selectedFile.name}`) ? 'No' : 'Yes'}</span
+									>{!fetchIncludeList.includes(`${$currentRoot}/${selectedFile.name}`)
+										? 'No'
+										: 'Yes'}</span
 								>
 							</div>
 							<div class="flex gap-2 w-full dark:text-white">
@@ -695,7 +685,7 @@
 							</div>
 						</div>
 						<Button on:click={() => showInExplorer(selectedFile)}>Show in Explorer</Button>
-						{#if selectedFile.lfsState === LocalFileLFSState.Stub || !inFetchInclude(`${$currentRoot}/${selectedFile.name}`)}
+						{#if selectedFile.lfsState === LocalFileLFSState.Stub || !fetchIncludeList.includes(`${$currentRoot}/${selectedFile.name}`)}
 							<Button
 								class="w-full"
 								color="primary"
