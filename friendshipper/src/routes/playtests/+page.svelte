e29d589f8f1d7999155bfbe39b4321dff35b6990
Andrew Rudoi <arudoi@believer.gg>
2024-11-04 10:02:29 -0800

    chore(friendshipper): fetch latest artifacts before popping server/playtest modals

--- friendshipper/src/routes/playtests/+page.svelte
+++ friendshipper/src/routes/playtests/+page.svelte
@@ -10,21 +10,33 @@
 	import PlaytestCard from '$lib/components/playtests/PlaytestCard.svelte';
 	import PlaytestModal from '$lib/components/playtests/PlaytestModal.svelte';
 	import { handleError } from '$lib/utils';
+	import { getBuilds } from '$lib/builds';
 
 	let loading = false;
+	let loadingMessage = 'Loading...';
 	let showModal = false;
 	let selectedPlaytest: Playtest | null = null;
 	let modalMode = ModalState.Creating;
 
-	const handleCreatePlaytest = () => {
+	const handleCreatePlaytest = async () => {
+		loadingMessage = 'Fetching latest builds...';
+		loading = true;
 		modalMode = ModalState.Creating;
 		selectedPlaytest = null;
+		builds.set(await getBuilds(250));
+		loading = false;
+
 		showModal = true;
 	};
 
-	const handleEditPlaytest = (playtest: Playtest | null) => {
+	const handleEditPlaytest = async (playtest: Playtest | null) => {
+		loadingMessage = 'Fetching latest builds...';
+		loading = true;
 		modalMode = ModalState.Editing;
 		selectedPlaytest = playtest;
+		builds.set(await getBuilds(250));
+		loading = false;
+
 		showModal = true;
 	};
 
@@ -60,22 +72,26 @@
 	<Button
 		class="!p-1.5"
 		size="xs"
-		on:click={() => {
-			handleCreatePlaytest();
+		disabled={loading}
+		on:click={async () => {
+			await handleCreatePlaytest();
 		}}
 	>
 		<CirclePlusOutline class="w-4 h-4" />
 	</Button>
 	{#if loading}
-		<Spinner size="4" />
+		<div class="flex items-center gap-2">
+			<Spinner size="4" />
+			<code class="text-xs text-gray-400 dark:text-gray-400">{loadingMessage}</code>
+		</div>
 	{/if}
 </div>
 <div class="flex flex-col gap-2 mb-2 overflow-auto">
 	{#each $playtests as playtest}
 		<PlaytestCard
 			{playtest}
-			handleEditPlaytest={() => {
-				handleEditPlaytest(playtest);
+			handleEditPlaytest={async () => {
+				await handleEditPlaytest(playtest);
 			}}
 			bind:loading
 		/>
--- friendshipper/src/routes/servers/+page.svelte
+++ friendshipper/src/routes/servers/+page.svelte
@@ -4,14 +4,16 @@
 	import { onMount } from 'svelte';
 	import { CirclePlusOutline, RefreshOutline } from 'flowbite-svelte-icons';
 	import { get } from 'svelte/store';
-	import { appConfig, selectedCommit } from '$lib/stores';
+	import { appConfig, builds, selectedCommit } from '$lib/stores';
 	import type { ArtifactEntry, GameServerResult, Nullable } from '$lib/types';
 	import ServerTable from '$lib/components/servers/ServerTable.svelte';
 	import { getServers } from '$lib/gameServers';
 	import ServerModal from '$lib/components/servers/ServerModal.svelte';
 	import { handleError } from '$lib/utils';
+	import { getBuilds } from '$lib/builds';
 
 	let fetchingServers = false;
+	let loadingMessage = 'Fetching servers...';
 	let servers: GameServerResult[] = [];
 
 	// create server modal
@@ -20,6 +22,7 @@
 
 	const updateServers = async () => {
 		if ($appConfig.initialized) {
+			loadingMessage = 'Fetching servers...';
 			fetchingServers = true;
 			try {
 				servers = await getServers();
@@ -72,7 +75,12 @@
 		disabled={fetchingServers}
 		class="!p-1.5"
 		primary
-		on:click={() => {
+		on:click={async () => {
+			loadingMessage = 'Fetching latest builds...';
+			fetchingServers = true;
+			builds.set(await getBuilds(250));
+			fetchingServers = false;
+
 			showModal = true;
 		}}
 	>
@@ -82,6 +90,9 @@
 			<CirclePlusOutline class="w-4 h-4" />
 		{/if}
 	</Button>
+	{#if fetchingServers}
+		<code class="text-xs text-gray-400 dark:text-gray-400">{loadingMessage}</code>
+	{/if}
 </div>
 <Card
 	class="w-full p-2 sm:p-2 max-w-full bg-secondary-700 dark:bg-space-900 h-full overflow-y-hidden border-0 shadow-none flex flex-col gap-0 overflow-auto"
