8cd5fc6d5a2d6d5f6afa86bc6cc7663b0404b286
Reuben Dunnington <4075241+rdunnington@users.noreply.github.com>
2024-12-12 16:19:18 -0800

    chore(friendshipper): add open folder button to servers page (#376)

--- friendshipper/src/routes/servers/+page.svelte
+++ friendshipper/src/routes/servers/+page.svelte
@@ -2,12 +2,12 @@
 	import { Button, Card, Spinner } from 'flowbite-svelte';
 	import { emit, listen } from '@tauri-apps/api/event';
 	import { onMount } from 'svelte';
-	import { CirclePlusOutline, RefreshOutline } from 'flowbite-svelte-icons';
+	import { CirclePlusOutline, RefreshOutline, FolderOpenOutline } from 'flowbite-svelte-icons';
 	import { get } from 'svelte/store';
 	import { appConfig, builds, selectedCommit } from '$lib/stores';
 	import type { ArtifactEntry, GameServerResult, Nullable } from '$lib/types';
 	import ServerTable from '$lib/components/servers/ServerTable.svelte';
-	import { getServers } from '$lib/gameServers';
+	import { getServers, openLogsFolder } from '$lib/gameServers';
 	import ServerModal from '$lib/components/servers/ServerModal.svelte';
 	import { handleError } from '$lib/utils';
 	import { getBuilds } from '$lib/builds';
@@ -55,44 +55,52 @@
 	});
 </script>
 
-<div class="flex items-center gap-2">
-	<p class="text-2xl my-2 text-primary-400 dark:text-primary-400">Servers</p>
-	<Button
-		disabled={fetchingServers}
-		class="!p-1.5"
-		primary
-		on:click={async () => {
-			await updateServers();
-		}}
-	>
-		{#if fetchingServers}
-			<Spinner size="4" />
-		{:else}
-			<RefreshOutline class="w-4 h-4" />
-		{/if}
-	</Button>
-	<Button
-		disabled={fetchingServers}
-		class="!p-1.5"
-		primary
-		on:click={async () => {
-			loadingMessage = 'Fetching latest builds...';
-			fetchingServers = true;
-			builds.set(await getBuilds(250));
-			fetchingServers = false;
+<div class="flex items-center justify-between gap-2">
+	<div class="flex items-center gap-2">
+		<p class="text-2xl my-2 text-primary-400 dark:text-primary-400">Servers</p>
+		<Button
+			disabled={fetchingServers}
+			class="!p-1.5"
+			primary
+			on:click={async () => {
+				await updateServers();
+			}}
+		>
+			{#if fetchingServers}
+				<Spinner size="4" />
+			{:else}
+				<RefreshOutline class="w-4 h-4" />
+			{/if}
+		</Button>
+		<Button
+			disabled={fetchingServers}
+			class="!p-1.5"
+			primary
+			on:click={async () => {
+				loadingMessage = 'Fetching latest builds...';
+				fetchingServers = true;
+				builds.set(await getBuilds(250));
+				fetchingServers = false;
 
-			showModal = true;
-		}}
-	>
+				showModal = true;
+			}}
+		>
+			{#if fetchingServers}
+				<Spinner size="4" />
+			{:else}
+				<CirclePlusOutline class="w-4 h-4" />
+			{/if}
+		</Button>
 		{#if fetchingServers}
-			<Spinner size="4" />
-		{:else}
-			<CirclePlusOutline class="w-4 h-4" />
+			<code class="text-xs text-gray-400 dark:text-gray-400">{loadingMessage}</code>
 		{/if}
-	</Button>
-	{#if fetchingServers}
-		<code class="text-xs text-gray-400 dark:text-gray-400">{loadingMessage}</code>
-	{/if}
+	</div>
+	<div class="flex items-center gap-2">
+		<Button outline class="!p-1.5" size="xs" on:click={openLogsFolder}>
+			<FolderOpenOutline class="w-4 h-4 mr-1" />
+			Open logs folder
+		</Button>
+	</div>
 </div>
 <Card
 	class="w-full p-2 sm:p-2 max-w-full bg-secondary-700 dark:bg-space-900 h-full overflow-y-hidden border-0 shadow-none flex flex-col gap-0 overflow-auto"
