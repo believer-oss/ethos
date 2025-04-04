b97de602f0460b01417b43f9b0cd1e82ab964835
Reuben Dunnington <rdunnington@believer.gg>
2025-04-04 10:03:41 -0700

    fix(friendshipper): invalid clipboard API usage
    * Turns out you have to use navigator.clipboard.* functions instead of importing them
      directly from the tauri plugin.

--- friendshipper/src/lib/components/workflows/WorkflowLogsModal.svelte
+++ friendshipper/src/lib/components/workflows/WorkflowLogsModal.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { Button, Hr, Input, Modal, Spinner, Tooltip } from 'flowbite-svelte';
 	import { emit } from '@tauri-apps/api/event';
-	import { writeText } from '@tauri-apps/plugin-clipboard-manager';
 	import { FileCopySolid } from 'flowbite-svelte-icons';
 	import type { JunitOutput, Nullable, Workflow, WorkflowNode } from '$lib/types';
 	import { getWorkflowJunitArtifact, getWorkflowNodeLogs } from '$lib/builds';
@@ -124,7 +123,7 @@
 
 	const copyLogToClipboard = async () => {
 		try {
-			await writeText(rawLogs);
+			await navigator.clipboard.writeText(rawLogs);
 		} catch (e) {
 			await emit('error', e);
 		}
