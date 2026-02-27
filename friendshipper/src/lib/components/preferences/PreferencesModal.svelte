a4fbef1c431132260a7108ff926a011908bbf8d1
Coho <matthew.w.gross+github@gmail.com>
2026-02-27 08:16:50 -0700

    fix(friendshipper): fix scrollwheel for preferences UI

--- friendshipper/src/lib/components/preferences/PreferencesModal.svelte
+++ friendshipper/src/lib/components/preferences/PreferencesModal.svelte
@@ -415,8 +415,8 @@
 </script>
 
 <Modal
-	defaultClass="bg-secondary-700 dark:bg-space-900 overflow-y-auto mb-16"
-	bodyClass="!border-t-0"
+	defaultClass="relative flex flex-col bg-secondary-700 dark:bg-space-900 h-[calc(100vh-6rem)]"
+	bodyClass="!border-t-0 pb-16"
 	backdropClass="fixed mt-8 inset-0 z-40 bg-gray-900 bg-opacity-50 dark:bg-opacity-80"
 	dialogClass="fixed mt-8 top-0 start-0 end-0 h-modal md:inset-0 md:h-full z-50 w-full p-4 pb-12 flex"
 	bind:open={showModal}
